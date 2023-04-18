8d0bd1a8df2404d6a1c20fe8705c7e716854faf4
TT <tanusaakovleva07@gmail.com>
2023-04-18 18:36:05 +0300

    Реализовала импорт данных, а также экспорт данных из базы данных в новый файл с возможностью выбора сортировки таблицы

--- Template_4337/Yakovleva.xaml.cs
+++ Template_4337/Yakovleva.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using OfficeOpenXml;
 
 namespace Template_4337
 {
@@ -23,5 +27,62 @@ namespace Template_4337
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Создание объекта для чтения xlsx-файла
+            var package = new ExcelPackage(new FileInfo("C:\\Users\\tanus\\OneDrive\\Рабочий стол\\1.xlsx"));
+
+            // Открытие соединения с базой данных
+            using (t1Entities1 connection = new t1Entities1())
+            {
+                // Обход строк в xlsx-файле
+                var worksheet = package.Workbook.Worksheets[1];
+                for (int row = 2; row <= 13; row++)
+                {
+                    // Чтение данных из xlsx-файла
+                    double id = Convert.ToDouble(worksheet.Cells[row, 1].Value);
+                    string kodZakaza = (worksheet.Cells[row, 2].Value.ToString());
+                    double dataSozdaniya = Convert.ToDouble(worksheet.Cells[row, 3].Value);
+                    
+
+
+
+                    myTable заказы = new myTable(id, kodZakaza, dataSozdaniya);
+                    connection.myTables.Add(заказы);
+                    connection.SaveChanges();
+                    MessageBox.Show("Success! Data has been added to database!");
+                }
+
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+            using (var connection = new SqlConnection("data source=LAPTOP-KH7CD52O;initial catalog=t1;integrated security=True;MultipleActiveResultSets=True"))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT [ID], [Наименование услуги], [Стоимость]\r\nFROM [t1].[dbo].[myTable]\r\nORDER BY [Стоимость]\r\n\r\n\r\n\r\n\r\n", connection);
+                // Выборка данных из базы данных
+                if ((bool)rd2.IsChecked)
+                {
+                     command = new SqlCommand("SELECT [ID], [Наименование услуги], [Стоимость]\r\nFROM [t1].[dbo].[myTable]\r\nORDER BY [Наименование услуги]\r\n\r\n\r\n\r\n\r\n", connection);
+                }
+                var dataReader = command.ExecuteReader();
+
+                // Создание файла Excel и заполнение его данными
+                var excelPackage = new ExcelPackage();
+                var worksheet = excelPackage.Workbook.Worksheets.Add("Worksheet Name");
+                worksheet.Cells.LoadFromDataReader(dataReader, true);
+
+                // Сохранение файла Excel на диск
+                var file = new FileInfo("C:\\Users\\tanus\\OneDrive\\Рабочий стол\\ExportedFile.xlsx");
+                excelPackage.SaveAs(file);
+
+                MessageBox.Show("Данные экспортированы успешно!");
+            }
+        }
     }
 }
+    
